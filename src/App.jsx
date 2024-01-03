c9354dc4c749c9a8817aa614cf611e7c0e1f33ec
dorrit <radithamzah12@gmail.com>
2024-01-03 14:22:39 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,115 @@
+import { useState } from "react";
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Form />
+      <Grocery />
+      <Footer />
+    </div>
+  );
+  
+
+}
+
+
+function Header() {
+  return <h1>Catatan Belanjaku 📝</h1>;
+}
+
+function Form() {
+
+  const [name, setName] = useState('')
+  const [quantity, setQuantity] = useState(1)
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = { name, quantity, checked: false, id: Date.now()}
+
+    console.log(newItem)
+
+  }
+
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i+1} key={i + 1}>{i + 1}</option>
+  ))
+
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>Hari ini belanja apa kita?</h3>
+      <div>
+        <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+          {quantityNum}
+        </select>
+        <input type="text" placeholder="nama barang..." value={name} onChange={(e) => setName(e.target.value)} />
+      </div>
+      <button>Tambah</button>
+    </form>
+  );
+}
+
+function Grocery() {
+  return (
+    <>
+    <div className="list">
+        <ul>
+          {groceryItems.map((item) => (
+            <Item item={item} key={item.id}/ >
+            ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button>Bersihkan Daftar</button>
+      </div>
+    </>
+  )
+}
+
+function Item({item}){
+  return (
+    <li key={item.id}>
+      <input type="checkbox" />
+      <span style={item.checked ? {textDecoration: "line-through"} : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>&times;</button>
+    </li>
+  );
+}
+
+
+function Footer() {
+  return <footer className="stats">
+    Ada 10 barang di daftar belanjaan, 5 barang sudah dibeli (50%)
+  </footer>;
+}
